2ac12eefd2da8c2730cd932207ccaf4ff4c833d9
dangvancuong196 <dangvancuong196@gmail.com>
2018-05-04 12:26:47 +0700

    view

--- application/views/footer.php
+++ application/views/footer.php
@@ -7,9 +7,11 @@
                     <li>Địa Chỉ: Học viện Công nghệ Bưu chính Viễn thông</li>
                     <li>Điện Thoại: 0988.192.713</li>
                     <li>Email: tungthai13@gmail.com abc</li>
+                    <li>tungthai13/thai09tung10</li>
                 </ul>
             </div>
 
         </div>
     </div>
 </div>
+
