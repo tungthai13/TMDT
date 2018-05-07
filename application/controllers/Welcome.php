e8456bba8179a78b114bbda5dd32e05aa5c6b104
Thai Hoang Tung <tungthai13@gmail.com>
2018-05-08 01:29:02 +0700

    Thay đổi database
    Cập nhật tính năng lọc kết quả hiển thị cửa hàng

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -23,41 +23,95 @@ class Welcome extends CI_Controller {
         $this->load->helper('url');
         $this->load->database();
         
+        //Lấy danh sách quận huyện từ procedure
         if (mysqli_more_results($this->db->conn_id)) {
             mysqli_next_result($this->db->conn_id);
         }
+        $data['danhSachQuanHuyen'] = $this->db->query("call danhSachQuanHuyen(?)", 1);
         
+        //Lấy danh sách quận huyện chọn trong phần lọc cửa hàng
+        $quanHuyen = $this->input->post("quanHuyen");
+        $data["quanHuyen"] = $quanHuyen; 
+        $stringMaQuanHuyen = ""; //Chuỗi chứa danh sách mã quận huyện theo dạng "1,5,6,9" để truyền vào trong param của procedure;
+        $tenQuanHuyen = []; //Mảng chứa danh sách tên các quận huyện đã chọn trong phần lọc cửa hàng
+        
+        //Kiểm tra $quanHuyen có phải là mảng không (TH có lọc cửa hàng)
+        if (is_array($quanHuyen) || is_object($quanHuyen)){
+            foreach ($quanHuyen as $item){
+                $stringMaQuanHuyen .= $item.","; //Thêm một mã quận huyện vào chuỗi
+                
+                //Lặp trong danh sách quận huyện để lấy ra tên quận huyện ứng với mã quận huyện $ỉtem
+                foreach ($data['danhSachQuanHuyen']->result() as $row){
+                    if($item == $row->ma_quan_huyen){
+                        //Đưa tên quận huyện vào mảng $tenQuanHuyen
+                        array_push($tenQuanHuyen, $row->ten_quan_huyen);
+                        break;
+                    }
+                }
+            } 
+            $stringMaQuanHuyen =  substr($stringMaQuanHuyen, 0, -1); //Xóa dấu phẩy cuối cùng trong chuỗi
+        }
+        
+        //Nối các tên quận huyện trong mảng vào biến $data["stringTenQuanHuyen"] theo định dạng: VD "Quận Hà Đông, Quận Thanh Xuân, Quận Hai Bà Trưng" để truyền vào param của procedure
+        $data["stringTenQuanHuyen"] = "";
+        foreach($tenQuanHuyen as $row){
+            $data["stringTenQuanHuyen"] .= $row.", "; //=> sẽ thừa dấu , và dấu cách ở cuối cùng
+        }
+        $data["stringTenQuanHuyen"] =  substr($data["stringTenQuanHuyen"], 0, -1); //Xóa dấu cách cuối
+        $data["stringTenQuanHuyen"] =  substr($data["stringTenQuanHuyen"], 0, -1); //Xóa dấu , cuối
+        
+        
+        if (mysqli_more_results($this->db->conn_id)) {
+            mysqli_next_result($this->db->conn_id);
+        }
+        //Số cửa hàng hiển thị trong 1 trang là 9
         $soPhanTuTrongMotTrang = 9;
         
+        //Đếm tổng số lượng cửa hàng trên hệ thống
         $query = $this->db->query("call tongSoCuaHang()");
         $data["tongSoCuaHang"] = $query->row()->tongSoCuaHang;
+        
+        //Tính toán số lượng trang hiển thị cửa hàng = tổng số cửa hàng / số cửa hàng trên 1 trang
         $data["tongSoTrang"] = $data["tongSoCuaHang"] / $soPhanTuTrongMotTrang;
+    
         if ($data["tongSoTrang"] <= 1) {
             $data["tongSoTrang"] = 1;
         } else if ($data["tongSoTrang"] % $soPhanTuTrongMotTrang != 0) {
             $data["tongSoTrang"] += 1;
         }
         
+        //Lấy ra trang hiển thị hiện tại
         $trangHienTai = $this->input->get("trangHienTai");
+        // Nếu chưa chọn trang thì mặc định hiển thị trang số 1
         if ($trangHienTai == null) {
             $trangHienTai = "1";
         } 
         $data["trangHienTai"] = $trangHienTai;
+        
         if (mysqli_more_results($this->db->conn_id)) {
             mysqli_next_result($this->db->conn_id);
         }
+        //TH không lọc cửa hàng
+        if($stringMaQuanHuyen == ""){
+            $array["phanTuBatDau"] = (intval($trangHienTai) - 1)*$soPhanTuTrongMotTrang;
+            $array["phanTuTrongMotTrang"] = $soPhanTuTrongMotTrang;     
+            
+            $data['danhSachCuaHang'] = $this->db->query("call tatCaCuaHang(?,?)", $array);
+        } else {
+        //TH có lọc cửa hàng
+            $array["array"] = $stringMaQuanHuyen;
+            $array["phanTuBatDau"] = (intval($trangHienTai) - 1)*$soPhanTuTrongMotTrang;
+            $array["phanTuTrongMotTrang"] = $soPhanTuTrongMotTrang;     
+            
+            $data['danhSachCuaHang'] = $this->db->query("call locCuaHangTheoQuanHuyen(?,?,?)", $array);
+        }
         
-        $arr["phanTuBatDau"] = (intval($trangHienTai) - 1)*$soPhanTuTrongMotTrang;
-        $arr["phanTuTrongMotTrang"] = $soPhanTuTrongMotTrang;     
-        
-        $data['danhSachCuaHang'] = $this->db->query("call tatCaCuaHang(?,?)", $arr);
-        
+        //Lấy ra 3 cửa hàng mới nhất trong hệ thống để hiện ra trên slide
         if (mysqli_more_results($this->db->conn_id)) {
             mysqli_next_result($this->db->conn_id);
         }
         $data['danhSachCuaHangSlide'] = $this->db->query("call danhSachCuaHangSlide()");
-        
-        
+               
 		$this->load->view('welcome_message', $data);
 	}
 }
--- application/views/content.php
+++ application/views/content.php
@@ -1,5 +1,5 @@
 <div id="content">
-            <div class="container" id="container1">
+    <div class="container" id="container1">
 
         <!--Tab-->
         <div class="row select">
@@ -7,10 +7,10 @@
                 <div class="tab">
                     <ul id="tabItem">
                         <li class="London active-slide" id='London1' onclick="openCity('London')">
-                            <span class="w3-bar-item w3-button London">Cửa hàng</span>
+                            <span class="btn London">Cửa hàng</span>
                         </li>
                         <li class="Tokyo" id='Tokyo1' onclick="openCity('Tokyo')">
-                            <span class="w3-bar-item w3-button Tokyo"> Phân loại</span>
+                            <span class="btn Tokyo"> Lọc kết quả hiển thị</span>
                         </li>
                     </ul>
                 </div>
@@ -20,91 +20,140 @@
 
         <!--Tab 1 content-->
         <div class="row product w3-container w3-display-container city tabLondon" id="London">
-           <?php
+            <?php 
+                //Kiểm tra xem có chọn quận huyện nào không
+                if (is_array($quanHuyen)){
+            ?>
+            <div class="col-md-12">
+                <h2>Lọc kết quả hiển thị theo quận huyện:
+                    <?php echo $stringTenQuanHuyen ?> </h2>
+            </div>
+            <?php } ?>
+            <?php
                 foreach( $danhSachCuaHang->result() as $row ) :
                     
             ?>
-            
-            
-            <!--Store Item-->
-            <div class="col-md-4 default">
-                <div class="item-product">
-                    <div class="image">
-                        <a href="<?php echo base_url();?>DatMon/index?maCuaHang=<?php echo $row->ma_cua_hang ?>"><img src="<?php echo base_url() ?>image/<?php echo $row->logo ?>" alt=""></a>
 
-                    </div>
-                    <div class="title">
-                        <a href="#">
-                            <div class="star">
-                                <span><?php 
+
+                <!--Store Item-->
+                <div class="col-md-4 default">
+                    <div class="item-product">
+                        <div class="image">
+                            <a href="<?php echo base_url();?>DatMon/index?maCuaHang=<?php echo $row->ma_cua_hang ?>"><img src="<?php echo base_url() ?>image/<?php echo $row->logo ?>" alt=""></a>
+
+                        </div>
+                        <div class="title">
+                            <a href="#">
+                                <div class="star">
+                                    <span><?php 
                                         echo ($row->tong_diem/$row->so_luot_cham)
                                         ?>
                                 </span>
-                            </div>
-                            <div class="address">
-                                <strong><?php echo $row->ten_cua_hang ?></strong><br>
-                                <span>
+                                </div>
+                                <div class="address">
+                                    <strong><?php echo $row->ten_cua_hang ?></strong><br>
+                                    <span>
                                     <?php echo $row->dia_chi ?>
                                 </span>
-                            </div>
+                                </div>
 
-                        </a>
+                            </a>
+                        </div>
+                        <div class="cach"></div>
+                        <div class="order">
+                            <!--                       onsubmit="return kiemTraDangNhap();"-->
+                            <form action="<?php echo base_url();?>DatMon/index?maCuaHang=<?php echo $row->ma_cua_hang ?>" method="post">
+
+                                <input type="hidden" name="maTaiKhoan" class="maTaiKhoan" value="1" />
+
+                                <input type="submit" class="storeButton btn btn-submit" value="Đặt món" />
+                            </form>
+                        </div>
+                        <!--End button-->
+
                     </div>
-                    <div class="cach"></div>
-                    <div class="order">
-<!--                       onsubmit="return kiemTraDangNhap();"-->
-                        <form action="<?php echo base_url();?>DatMon/index?maCuaHang=<?php echo $row->ma_cua_hang ?>" method="post">
-                            
-                            <input type="hidden" name="maTaiKhoan" class="maTaiKhoan" value="1" />
-
-                            <input type="submit" class="storeButton btn btn-submit" value="Đặt món" />
-                        </form>
-                    </div>
-                    <!--End button-->
-
                 </div>
-            </div>
-            <?php
+                <?php
                 endforeach;
             ?>
-            <!--End Store Item-->
-            <div class="col-md-12" align="center">
-                <nav aria-label="Page navigation example">
-                    <ul class="pagination justify-content-center">
-                        <li class="page-item">
-                            <?php if($trangHienTai == 1){ ?>
-                            <a class="page-link" href="<?php echo base_url() ?>Welcome/index?trangHienTai=1" tabindex="-1">Previous</a>
-                            <?php } else { ?>
-                            <a class="page-link" href="<?php echo base_url() ?>Welcome/index?trangHienTai=<?php echo $trangHienTai-1; ?>" tabindex="-1">Previous</a>
-                            <?php } ?>
-                        </li>
-                        <?php for($i = 1; $i <= $tongSoTrang; $i++): ?>
-                        <li class="page-item"><a class="page-link" href="<?php echo base_url() ?>Welcome/index?trangHienTai=<?php echo $i?>"><?php echo $i?></a></li>
-                        <?php endfor; ?>
-                        <li class="page-item">
-                            <?php if($trangHienTai == $tongSoTrang){ ?>
-                            <a class="page-link" href="<?php echo base_url() ?>Welcome/index?trangHienTai=<?php echo $tongSoTrang; ?>">Next</a>
-                            <?php } else { ?>
-                            <a class="page-link" href="<?php echo base_url() ?>Welcome/index?trangHienTai=<?php echo $trangHienTai+1; ?>">Next</a>
-                            <?php } ?>                           
-                        </li>
-                    </ul>
-                </nav>
-            </div>
+                    <!--End Store Item-->
+                    <div class="col-md-12" align="center">
+                        <nav aria-label="Page navigation example">
+                            <ul class="pagination justify-content-center">
+                                <li class="page-item">
+                                    <?php if($trangHienTai == 1){ ?>
+                                    <a class="page-link" href="<?php echo base_url() ?>Welcome/index?trangHienTai=1" tabindex="-1">Previous</a>
+                                    <?php } else { ?>
+                                    <a class="page-link" href="<?php echo base_url() ?>Welcome/index?trangHienTai=<?php echo $trangHienTai-1; ?>" tabindex="-1">Previous</a>
+                                    <?php } ?>
+                                </li>
+                                <?php for($i = 1; $i <= $tongSoTrang; $i++): ?>
+                                <li class="page-item">
+                                    <a class="page-link" href="<?php echo base_url() ?>Welcome/index?trangHienTai=<?php echo $i?>">
+                                        <?php echo $i?>
+                                    </a>
+                                </li>
+                                <?php endfor; ?>
+                                <li class="page-item">
+                                    <?php if($trangHienTai == $tongSoTrang){ ?>
+                                    <a class="page-link" href="<?php echo base_url() ?>Welcome/index?trangHienTai=<?php echo $tongSoTrang; ?>">Next</a>
+                                    <?php } else { ?>
+                                    <a class="page-link" href="<?php echo base_url() ?>Welcome/index?trangHienTai=<?php echo $trangHienTai+1; ?>">Next</a>
+                                    <?php } ?>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
         </div>
 
         <!--End tab 1 content-->
 
         <!--Tab 3 content-->
         <div class="row product w3-container w3-display-container city" id="Tokyo" style="display:none">
-            <div class="col-md-12">
-                <h2>Khu vực</h2>
-            </div>
-            <div class="col-md-12">
-                <form>
-                    <input type="checkbox" value=""> Quận Hà Đông &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    <input type="checkbox" value=""> Quận Hà Đông &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    <input type="checkbox" value=""> Quận Hà Đông &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <div class="col-xs-6">
+                <form action="<?php echo base_url(); ?>Welcome/index" method="post">
+                    <h3 class="text-center">Lọc cửa hàng theo quận huyện</h3>
+                    <div class="well" style="max-height: 550px;overflow: auto;">
+                        <ul class="list-group checked-list-box">
+
+
+                            <?php 
+                                if (!is_array($quanHuyen)){
+                                    $quanHuyen = [];
+                                }
+                            
+                                foreach($danhSachQuanHuyen->result() as $row): 
+                            ?>
+
+                            <?php if (in_array($row->ma_quan_huyen, $quanHuyen)){ ?>
+                            <li class="list-group-item">
+                                <input checked type="checkbox" name="quanHuyen[]" value="<?php echo $row->ma_quan_huyen ?>">
+                                <?php echo $row->ten_quan_huyen ?>
+                            </li>
+                            <?php } else { ?>
+                            <li class="list-group-item">
+                                <input type="checkbox" name="quanHuyen[]" value="<?php echo $row->ma_quan_huyen ?>">
+                                <?php echo $row->ten_quan_huyen ?>
+                            </li>
+                            <?php } ?>
+
+                            <?php endforeach; ?>
+                            
+
+                        </ul>
+                    </div>
+                    <br>
+                    <input type="submit" value="Lọc kết quả" class="btn btn-success" />
+                    &nbsp;&nbsp;
+                    <input type="reset" value="Bỏ chọn" id="reset" class="btn btn-danger" onclick="myFunction()" />
+                    <script>
+                        function myFunction(){
+                            var checkBoxes = document.getElementsByName("quanHuyen[]");
+                            for(var i=0; i<checkBoxes.length; i++){
+                                checkBoxes[i].removeAttribute("checked");
+                            }
+                        }
+                    </script>
                 </form>
             </div>
 
